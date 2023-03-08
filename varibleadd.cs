3465d97df922d81924622807a9ca2378c9d50604
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-08 20:44:10 +0530

    varible add

---
+++ varibleadd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace logicassigmentsolve
+{
+     public class varibleadd
+    {
+
+
+        public static void resultshow()
+        {
+            int a = 10;
+            int b = 20;
+            double numl = 2.2;
+            char ch = 'a';
+            bool val = false;
+            string str = "Bridgelabz";
+
+
+            Console.WriteLine("The integer value is " + a + b);
+            Console.WriteLine("The double value is " + numl);
+            Console.WriteLine("The char value is " + ch);
+            Console.WriteLine("The bool value is " + val);
+            Console.WriteLine("The string value is " + str);
+            Console.WriteLine(a + b);
+        }
+    }
+
+}
+
+
