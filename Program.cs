c514dbc7500903426f9e9e13c7d05730f298e88b
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-08 20:35:01 +0530

    first to git

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace logicassigmentsolve
+{ 
+
+    class program
+    {
+        public static void Main(string[]args)
+        {
+            //heapcoin.FlipTheCoin();
+            //varibleadd.resultshow();
+            //vribleadd3.sumofnumber();
+             leapyear.checkyear();
+
+        }
+    }
+}
