fa03dbb09fada48bd20337490cd5a111fb5a72da
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-08 20:47:19 +0530

    addding count value

---
+++ vribleadd3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicassigmentsolve
+{
+    
+         public  class vribleadd3
+        {
+
+
+        public static void sumofnumber()
+        {
+
+
+                Console.WriteLine("PLZ Enter the first number");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("PLZ Enter the second number");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int num3 = num1 + num2;
+                Console.WriteLine("The sum of two number" + num3);
+            }
+        }
+
+    }
+
+
+
+    
+    
+
